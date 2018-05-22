6afa70ada096f8f3cdaf6b88fa4b9d2da883a595
aeffiong <aeffiong9@gmail.com>
2018-05-21 21:01:39 -0600

    psuedocoding

---
+++ assests/javascript/app.js
@@ -0,0 +1,9 @@
+// define the variables including the array or topic strings and the api url
+
+// start creating functions: we will need:
+// document on ready
+// a function that displays the gifs when the button is clicked
+// a function that pushes the user input into the arry so that a button can be created on the screen to then be clicked
+// to display the list of gifs
+// lists the rating of each gif below the gif
+// start with a still image of the gif, user can click to start gif and click again to pause gif
